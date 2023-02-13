a582bff57637bfb20ef73a92ec4868a1f63b4f87
BGCG <billie.griffith4@gmail.com>
2023-02-13 10:41:38 +0000

    Build functionality for viewing tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -13,4 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // select intialisation
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initialisation
+    let collapsible = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsible);
 });
